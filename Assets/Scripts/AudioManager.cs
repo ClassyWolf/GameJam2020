d87373830eed7a320dccf71fd425062065cb6f0d
ClassyWolf <oskar.mikael@gmail.com>
2020-02-01 20:28:00 +0200

    Added audio manager code

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioInfo[] audioClip;
+    public static AudioManager instance = null;
+    private float volume = 0.5f;
+
+    public float Volume
     {
-        
+        get { return volume; }
+        set
+        {
+            volume = value;
+            foreach (AudioInfo audioClip in audioClip)
+            {
+                audioClip.source.volume = value;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            foreach (AudioInfo sound in audioClip)
+            {
+                sound.source = gameObject.AddComponent<AudioSource>();
+                sound.source.clip = sound.clip;
+                sound.source.loop = sound.loop;
+                sound.source.volume = sound.volumeSlider;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    //void OnEnable()
+    //{
+    //    SceneManager.sceneLoaded += OnSceneLoaded;
+    //}
+
+    //void OnDisable()
+    //{
+    //    SceneManager.sceneLoaded -= OnSceneLoaded;
+    //}
+
+    //public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    //{
+    //    SwapMusic(scene.name);
+    //}
+
+    void SwapMusic(string name)
+    {
+        if (name == "Map1Scene" || name == "TestRoomScene" || name == "Oskar'sWorkScene")
+        {
+
+            audioClip[0].source.Play();
+        }
+        else
+        {
+            audioClip[0].source.Stop();
+        }
+    }
+
+    public void PlayEfx(int clipNumber)
+    {
+        audioClip[clipNumber].source.PlayOneShot(audioClip[clipNumber].clip);
+    }
+}
+
+[System.Serializable]
+public class AudioInfo
+{
+    public string clipName;
+    public AudioClip clip;
+    public bool loop;
+    public float volumeSlider = 0.5f;
+    [HideInInspector] public AudioSource source;
 }
