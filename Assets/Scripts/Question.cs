27b2e02f5c78faac7819217b0d512ebb3ba790af
Munski <antti.muunoja@hotmail.fi>
2020-02-01 15:08:44 +0200

    Question scriptableobject

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Question", menuName = "Question")]
+public class Question : ScriptableObject
+{
+    public string joke;
+    public string rightAnswer;
+    public string wrongAnswer_1;
+    public string wrongAnswer_2;
+    public string wrongAnswer_3;
+}
